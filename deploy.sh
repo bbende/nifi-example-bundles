e2a41375a831f0154580c43df6288339d94102c5
bbende <bbende@gmail.com>
2015-05-05 19:41:14 -0400

    removing old nars/processors

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,9 @@ if [ -z "$NIFI_HOME" -a "${NIFI_HOME+xxx}" = "xxx" ]; then
 fi
 
 echo "Removing old nars from $NIFI_HOME/lib..."
-
 rm $NIFI_HOME/lib/nifi-example-utils-nar*
-rm $NIFI_HOME/lib/nifi-twitter-nar*
-rm $NIFI_HOME/lib/nifi-solr-nar*
 
 echo "Deploying latest nars to $NIFI_HOME/lib..."
-
 cp nifi-example-utils-bundle/nifi-example-utils-nar/target/nifi-example-utils-nar*.nar $NIFI_HOME/lib/
-cp nifi-twitter-bundle/nifi-twitter-nar/target/nifi-twitter-nar*.nar $NIFI_HOME/lib/
-cp nifi-solr-bundle//nifi-solr-nar/target/nifi-solr-nar*.nar $NIFI_HOME/lib/
 
 echo "DONE!"
